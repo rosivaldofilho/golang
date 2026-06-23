ba563da273551ea4ddbdca093af52ff9f64996a3
ROSIVALDO FREITAS DE SOUZA FILHO <rosivaldo.fh@gmail.com>
2026-06-23 10:37:30 -0300

    Características das Variáveis - Var

--- linguagem/variaveis/variavel.go
+++ linguagem/variaveis/variavel.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 // Variáveis do tipo Var e Const
+/*
+Variáveis possuem tipagem automática (inferência de tipo)
+caso seja setado o valor em uma variável que ainda não possui tipo;
+Inicialização opcional
+Se a variável não for inicializada,
+ela receberá o valor zero do tipo de dado
+
+*/
 
 func main() {
 	var nome = "Rosivaldo"
