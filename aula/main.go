8218f101f7813bb0dab0fb8bf9b17d3ac66d1fce
rosivaldofilho <rosivaldo.fh@gmail.com>
2026-06-23 10:00:43 -0300

    Primeiro commit

---
+++ aula/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numero = 30.10
+	var nome = "Matheus"
+	fmt.Println("Bem vindo meu primeiro código em GO")
+	fmt.Printf("Número %.3f \n", numero)
+	fmt.Println("Bem vindo ", nome)
+	fmt.Printf("Olá %s Bem vindo ao código em GO", nome)
+}
---
+++ aula/variaveis/variavel.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// Variáveis do tipo Var e Cons
+
+func main() {
+	fmt.Println("Variáveis")
+
+}
